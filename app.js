d030fb9af4dfc9cd69a3cb8c608073c028bb9731
saksham <sharma.saksham0309@gmail.com>
2024-04-27 00:04:51 +0530

    DONE03

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    orgin:[process.env.FRONTEND_URL],
+    origin:[process.env.FRONTEND_URL],
     methods:["GET","POST","PUT","DELETE"],
-    credentials: true,
+    credentials:true,
 }))
 
 //using routes
