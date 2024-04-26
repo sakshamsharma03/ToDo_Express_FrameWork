b4fcb6e50f4e1ff03655a3b984b9acbbd3453630
saksham <sharma.saksham0309@gmail.com>
2024-04-26 18:15:24 +0530

    Done

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { connectDB } from "./data/database.js";
 connectDB();
 
 
-app.listen( 4000,()=>
+app.listen(process.env.PORT || 4000,()=>
 {
     console.log(`Server is Workin on Port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`)
 })
